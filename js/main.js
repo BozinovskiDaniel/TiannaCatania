d2ca8c68aa68b8154e620d19e06f1cb5bc5d7332
BozinovsiDaniel <Bozinovskidaniel@hotmail.com>
2019-09-11 00:03:49 +1000

    latest

--- js/main.js
+++ js/main.js
@@ -7,3 +7,16 @@ $(window).scroll(function(){
     $(".arrow").css("opacity", 1 - $(window).scrollTop() / 600); 
   //250 is fade pixels
   });
+
+const about = document.getElementsByTagName('img')[1];
+const whole = document.getElementById('AMP');
+const topsec = document.getElementsByClassName('row')[0];
+
+
+const tl = new TimelineMax();
+
+// , ease: Power2.easeInOut
+
+tl
+.fromTo(whole, 1.5, {width: "0%"}, {width: "100%", ease: Power2.easeInOut})
+.fromTo(topsec, 3, {height: "0%"}, {height: "100%", ease: Power2.easeInOut});
